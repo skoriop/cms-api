358b01d17b52e4d4f94d0191f1d0d23d2c63b8b1
skoriop <karthikp.ivy@gmail.com>
2023-02-28 18:16:32 +0530

    Added self CRUD

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -7,6 +7,66 @@ import { User } from "../models/User";
 
 export const userRoute = Router();
 
+userRoute.get("/me", verifyAccessToken, async (req: any, res) => {
+	const user = await getCurrentUser(req);
+	if (user.userType === UserType.ADMIN) {
+		res.send({
+			id: user.id,
+			username: user.username,
+			name: user.name,
+			email: user.email,
+			type: user.userType,
+			courses: user.courses,
+		});
+	} else {
+		res.send({
+			username: user.username,
+			name: user.name,
+			email: user.email,
+			type: user.userType,
+			courses: user.courses,
+		});
+	}
+});
+
+userRoute.put("/me", verifyAccessToken, async (req: any, res) => {
+	const user = await getCurrentUser(req);
+
+	const is_username = await User.findOne({ username: req.body.username });
+	if (is_username) return res.status(400).send("Username already exists");
+
+	const is_email = await User.findOne({ email: req.body.email });
+	if (is_email) return res.status(400).send("Email already exists");
+
+	try {
+		const updatedUser = await User.findOneAndUpdate(
+			{ username: user.username },
+			{
+				username: req.body.username || user.username,
+				email: req.body.email || user.email,
+				name: req.body.name || user.name,
+			},
+			{ new: true }
+		);
+		res.send({
+			username: updatedUser.username,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			type: updatedUser.userType,
+			courses: updatedUser.courses,
+		});
+	} catch (err) {
+		return res.status(400).send(err);
+	}
+});
+
+userRoute.delete("/me", verifyAccessToken, async (req: any, res) => {
+	const user = await getCurrentUser(req);
+	await user.delete();
+	console.log(`Deleted user ${user.username}`);
+	return res.sendStatus(204);
+});
+
 userRoute.get("/:username", verifyAccessToken, async (req: any, res) => {
 	const user = await User.findOne({ username: req.params.username });
 	if (!user) return res.status(404).send("User not found");
@@ -56,7 +116,7 @@ userRoute.put("/:username", verifyAccessToken, async (req: any, res) => {
 		{ new: true }
 	);
 
-	return res.send(newUser);
+	res.send(newUser);
 });
 
 userRoute.delete("/:username", verifyAccessToken, async (req: any, res) => {
@@ -67,5 +127,5 @@ userRoute.delete("/:username", verifyAccessToken, async (req: any, res) => {
 	if (!user) return res.status(404).send("User not found");
 
 	console.log(`Deleted user ${req.params.username}`);
-	return res.sendStatus(204);
+	res.sendStatus(204);
 });
